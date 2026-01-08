c259b87e5815b39ddecc3b8537aa56fa0c8981f0
Chithraka Kalanamith <chithrakakalanamith@gmail.com>
2026-01-08 12:04:46 +0530

    backend url update

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 import LogoutButton from './LogoutButton';
 import { WiCloudy } from 'react-icons/wi';
 
-const baseURL = import.meta.env.BACKEND_URL || 'http://localhost:5000';
+const baseURL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:5000';
 
 const Dashboard = () => {
     const { getAccessTokenSilently } = useAuth0();
